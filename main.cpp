38899c8631872c47389666068f60eb920e885751
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2018-11-05 11:29:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,35 +8,35 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
-    SIGN obj[N];
+    SIGN *obj=new SIGN[N];
     //SIGN a("Popov", "Dmitriy", "Virgo", 27, 8, 1998);        cout<<a;
     system("cls");
     int choise;
     for(int i=0; i<N; i++)
-        cin>>obj[i];
+        cin>>*(obj+i);
     system("cls");
-    SIGN buf;
+    SIGN *buf=new SIGN;
     for(int i=0; i<N-1; i++)
     {
         for(int j=0; j<N-i-1; j++)
         {
-            if(Sort(obj[j], obj[j+1])) {
-                buf = obj[j];
-                obj[j] = obj[j+1];
-                obj[j+1] = buf;
+            if(Sort(obj+j, obj+j+1))
+            {
+                *buf = *(obj+j);
+                *(obj+j) = *(obj+j+1);
+                *(obj+j+1) = *buf;
             }
         }
     }
     while(1)
     {
-        system("cls");
         cout<<"1)Вывести все записи\n2)Вывести записи по знаку зодиака\n0)Выход\n>";
         cin>>choise;
         switch(choise)
         {
         case 1:
             for(int n=0; n<N; n++)
-                cout<<obj[n];
+                cout<<*(obj+n);
             break;
         case 2:
             {
@@ -48,7 +48,7 @@ int main()
             {
                 if(obj[i].show(zodiak))
                 {
-                    cout<<obj[i];
+                    cout<<*(obj+i);
                     flag=1;
                 }
             }
